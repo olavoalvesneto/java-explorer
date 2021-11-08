db22a8e81989c3d15159d9521d9db2c18b2e0d69
Olavo Alves <olavoalvesneto@hotmail.com>
2021-11-08 22:36:47 +0000

    Exercise 4-3, 4-4: Using a Loop and Control Flow an Array

--- src/duke/choice/ShopApp.java
+++ src/duke/choice/ShopApp.java
@@ -18,14 +18,16 @@ public class ShopApp {
         // TODO code application logic here
 
         Double tax = 0.2;
-        Double total;
+        Double total = 0.0;
         Customer c1 = new Customer();
         c1.setName("Pink");
         c1.setSize("S");
         System.out.println("Welcome to Duke Choice Shop " + c1.getName());
-        
+
         Clothing item1 = new Clothing();
         Clothing item2 = new Clothing();
+        Clothing item3 = new Clothing();
+        Clothing item4 = new Clothing();
 
         item1.setDescription("Blue Jacket");
         item1.setPrice(20.9);
@@ -33,16 +35,44 @@ public class ShopApp {
         item2.setDescription("Orange T-Shirt");
         item2.setPrice(10.50);
         item2.setSize("S");
-        
-        Clothing[] itens = {item1, item2};
 
-        System.out.println("Item1 : " + itens[0].getDescription() + "," + itens[0].getPrice() + "," + itens[0].getSize());
-        System.out.println("Item2 : " + itens[1].getDescription() + "," + itens[1].getPrice() + "," + itens[1].getSize());
+        item3.setDescription("Green Scarf");
+        item3.setPrice(5.0);
+        item3.setSize("S");
 
-        total = ((item1.getPrice() + (item2.getPrice() * 2))) * (1 + tax);
+        item4.setDescription("Blue T-Shirt");
+        item4.setPrice(10.50);
+        item4.setSize("S");
+
+        Clothing[] itens = {item1, item2, item3, item4};
+        int measurement = 3;
+
+        switch (measurement) {
+            case 1, 2, 3:
+                c1.setSize("S");
+                break;
+            case 4, 5, 6:
+                c1.setSize("M");
+                break;
+            case 7, 8, 9:
+                c1.setSize("L");
+                break;
+            default:
+                c1.setSize("X");
+        }
+
+        for (Clothing item : itens) {
+            if (c1.getSize().equals(item.getSize())) {
+                System.out.println("Item ," + item.getDescription() + "," + item.getPrice() + "," + item.getSize());
+                total += (item.getPrice() * (1 + tax));
+                
+                if (total > 15) {
+                    break;
+                }
+            }
+        }
         System.out.println("Total : " + total);
 
-        int measurement = 3;
     }
 
 }
