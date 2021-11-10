0503c31024b1ae25bdbfc3b34d0751214a9afce1
Olavo Alves <olavoalvesneto@hotmail.com>
2021-11-10 17:33:46 +0000

    Prepare for Cloud Deployment

--- src/duke/choice/ShopApp.java
+++ src/duke/choice/ShopApp.java
@@ -5,6 +5,12 @@
  */
 package duke.choice;
 
+import io.helidon.webserver.Routing;
+import io.helidon.webserver.ServerConfiguration;
+import io.helidon.webserver.WebServer;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import java.util.Arrays;
 
 /**
@@ -18,15 +24,15 @@ public class ShopApp {
      */
     public static void main(String[] args) {
 
-        double total = 0.0;
-        int average = 0;
-        int count = 0;
+  //      double total = 0.0;
+  //      int average = 0;
+  //      int count = 0;
 
-        Customer c1 = new Customer("Pink", 3);
+  //      Customer c1 = new Customer("Pink", 3);
 
         System.out.println("Welcome to Duke Choice Shop ");
 
-        System.out.println("Min. Price: " + Clothing.MIN_PRICE);
+        //System.out.println("Min. Price: " + Clothing.MIN_PRICE);
 
         Clothing item1 = new Clothing("Blue Jacket", 20.9, "M");
         Clothing item2 = new Clothing("Orange T-Shirt", 10.5, "S");
@@ -34,26 +40,44 @@ public class ShopApp {
         Clothing item4 = new Clothing("Blue T-Shirt", 10.5, "S");
 
         Clothing[] items = {item1, item2, item3, item4};
-        c1.addItems(items);
-
-        System.out.println("Customer is " + c1.getName() + " Size: " + c1.getSize() + "," + c1.getTotalClothingCost());
-
-        for (Clothing item : c1.getItems()) {
-            if (item.getSize().equals("L")) {
-                count++;
-                average += item.getPrice();
-            }
-        }
+        
+        
         try {
-            average = (count == 0) ? 0 : average / count;
-            System.out.println("Avergage price: " + average + ", Count " + count);
-        } catch (ArithmeticException e) {
-            System.out.println("Dont divide by zero");
+            ItemList list = new ItemList(items);
+            Routing routing = Routing.builder().get("/items", list).build();
+            ServerConfiguration config = ServerConfiguration.builder()
+                    .bindAddress(InetAddress.getLocalHost())
+                    .port(8888).build();
+            WebServer ws = WebServer.create(config, routing);
+            ws.start();
+            
+            
+        } catch (UnknownHostException ex) {
+            ex.printStackTrace();
         }
+        
+//        c1.addItems(items);
 
-        Arrays.sort(c1.getItems());
-        for (Clothing item : c1.getItems()) {
-            System.out.println("Items " + item);
-        }
+//        System.out.println("Customer is " + c1.getName() + " Size: " + c1.getSize() + "," + c1.getTotalClothingCost());
+
+//        for (Clothing item : c1.getItems()) {
+//            if (item.getSize().equals("L")) {
+//                count++;
+//                average += item.getPrice();
+//            }
+//        }
+
+
+//        try {
+//            average = (count == 0) ? 0 : average / count;
+//            System.out.println("Avergage price: " + average + ", Count " + count);
+//        } catch (ArithmeticException e) {
+//            System.out.println("Dont divide by zero");
+//        }
+
+//        Arrays.sort(c1.getItems());
+//        for (Clothing item : c1.getItems()) {
+//            System.out.println("Items " + item);
+//        }
     }
 }
